02c22b5e2e5071995fa2d48ff0c360e99d2f7922
Seongjin Lee <babo318@gmail.com>
2021-12-28 00:09:02 +0900

    초기 세팅

---
+++ models/menus.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const autoIncrement = require("mongoose-auto-increment");
+autoIncrement.initialize(mongoose.connection);
+
+const MenusSchema = new mongoose.Schema({
+    menu_id: {
+        type: Number,
+        required: true,
+        unique: true,
+        default: 0,
+    },
+    menuType: {
+        type: String,
+        required: true,
+    },
+    menuStyle: {
+        type: String,
+        required: true,
+    },
+    menuWith: {
+        type: String,
+        required: true,
+    },
+    menuResult: {
+        type: String,
+        required: true,
+    },
+    menuLikeCnt: {
+        type: Number,
+        required: true,
+    },
+    menuResultCnt: {
+        type: Number,
+        required: true,
+    },
+});
+
+MenusSchema.plugin(autoIncrement.plugin, {
+    model: "MenusSchema",
+    field: "menu_id",
+    startAt: 1,
+    increment: 1,
+});
+
+module.exports = mongoose.model("Menus", MenusSchema);
