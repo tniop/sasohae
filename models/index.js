4fbcdf74f9e50132365c62c82fff8d289ed8863e
Seongjin Lee <babo318@gmail.com>
2021-12-28 00:17:14 +0900

    DB연결 코드

---
+++ models/index.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const connect = () => {
+    //   mongoose
+    //     .connect('mongodb://test:test@localhost:27017/admin', {
+    mongoose
+        .connect("mongodb://localhost:27017/sasohae", {
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+            // ignoreUndefined: true,
+        })
+        .catch((err) => console.log(err));
+};
+
+mongoose.connection.on("error", (err) => {
+    console.error("몽고DB 연결 에러", err);
+});
+
+// 몽고디비 연결이 끊겼을 때
+mongoose.connection.on("disconnected", () => {
+    console.error("몽고디비 연결이 끊김. 연결을 재시도함");
+    connect();
+});
+
+module.exports = connect;
