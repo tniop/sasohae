aa6ac4c9b9adfd6da2765e9e45286a94c40c1735
Seongjin Lee <babo318@gmail.com>
2022-01-04 20:30:46 +0900

    response 응답 값 수정;

--- routes/controllers/ranking.js
+++ routes/controllers/ranking.js
@@ -1,7 +1,6 @@
-const {
-    updateGiftRanking,
-    updateMenuRanking,
-} = require("../../functions/rankingUpdate");
+const gifts = require("../../models/gifts");
+const menus = require("../../models/menus");
+const rankings = require("../../models/rankings");
 
 // const cron = require("node-cron");
 
@@ -10,7 +9,59 @@ const {
 
 async function getTopRankedGifts(req, res) {
     try {
-        updateGiftRanking();
+        const top10Ranked = await gifts
+            .find({})
+            .limit(10)
+            .sort({ giftLikeCnt: -1 });
+
+        const tempRankingArr = [];
+
+        const rankingDB = await rankings.findOne({ ranking_Id: 1 });
+
+        if (!rankingDB) {
+            for (let i = 0; i < top10Ranked.length; i++) {
+                tempRankingArr[i] = {};
+                const rank = i + 1;
+                const title = top10Ranked[i].giftName;
+                currentRankingSet.add(title);
+                tempRankingArr[i].rank = rank;
+                tempRankingArr[i].title = title;
+                tempRankingArr[i].variance = "New";
+            }
+
+            await rankings.create({
+                pastRanking: tempRankingArr,
+                currentRanking: tempRankingArr,
+            });
+        } else {
+            const pastRankingInDB = rankingDB.pastRanking;
+            const currentRankinginDB = rankingDB.currentRanking;
+
+            for (let i = 0; i < top10Ranked.length; i++) {
+                //현재 랭킹 생성;
+                tempRankingArr[i] = {};
+                const rank = i + 1;
+                const title = top10Ranked[i].giftName;
+                tempRankingArr[i].rank = rank;
+                tempRankingArr[i].title = title;
+                tempRankingArr[i].variance = "New";
+                for (let k = 0; k < pastRankingInDB.length; k++) {
+                    if (tempRankingArr[i].title == pastRankingInDB[k].title) {
+                        tempRankingArr[i].variance =
+                            pastRankingInDB[k].rank - tempRankingArr[i].rank;
+                    }
+                }
+            }
+
+            await rankings.updateOne(
+                { ranking_Id: 1 },
+                { $set: { pastRanking: currentRankinginDB } }
+            );
+            await rankings.updateOne(
+                { ranking_Id: 1 },
+                { $set: { currentRanking: tempRankingArr } }
+            );
+        }
         res.status(200).send(tempRankingArr);
     } catch (err) {
         console.log(err);
@@ -20,7 +71,58 @@ async function getTopRankedGifts(req, res) {
 
 async function getTopRankedMenus(req, res) {
     try {
-        updateMenuRanking();
+        const top10Ranked = await menus
+            .find({})
+            .limit(10)
+            .sort({ menuLikeCnt: -1 });
+
+        const tempRankingArr = [];
+
+        const rankingDB = await rankings.findOne({ ranking_Id: 2 });
+
+        if (!rankingDB) {
+            for (let i = 0; i < top10Ranked.length; i++) {
+                tempRankingArr[i] = {};
+                const rank = i + 1;
+                const title = top10Ranked[i].menuName;
+                tempRankingArr[i].rank = rank;
+                tempRankingArr[i].title = title;
+                tempRankingArr[i].variance = "New";
+            }
+
+            await rankings.create({
+                pastRanking: tempRankingArr,
+                currentRanking: tempRankingArr,
+            });
+        } else {
+            const pastRankingInDB = rankingDB.pastRanking;
+            const currentRankinginDB = rankingDB.currentRanking;
+
+            for (let i = 0; i < top10Ranked.length; i++) {
+                //현재 랭킹 생성;
+                tempRankingArr[i] = {};
+                const rank = i + 1;
+                const title = top10Ranked[i].menuName;
+                tempRankingArr[i].rank = rank;
+                tempRankingArr[i].title = title;
+                tempRankingArr[i].variance = "New";
+                for (let k = 0; k < pastRankingInDB.length; k++) {
+                    if (tempRankingArr[i].title == pastRankingInDB[k].title) {
+                        tempRankingArr[i].variance =
+                            pastRankingInDB[k].rank - tempRankingArr[i].rank;
+                    }
+                }
+            }
+
+            await rankings.updateOne(
+                { ranking_Id: 2 },
+                { $set: { pastRanking: currentRankinginDB } }
+            );
+            await rankings.updateOne(
+                { ranking_Id: 2 },
+                { $set: { currentRanking: tempRankingArr } }
+            );
+        }
         res.status(200).send(tempRankingArr);
     } catch (err) {
         console.log(err);
